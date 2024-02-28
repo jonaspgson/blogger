1e8766585a01b2e6fdfb32240c5511d6013929fd
jonaspgson <jonaspgson@gmail.com>
2024-02-29 00:45:15 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,23 @@
 //<![CDATA[
 
+/* Accordion - requires additional CSS. */
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+
 /* Requires Font-Awesome and additional CSS. */
 function rateThis(n) {
 	var startWrap = "<div class='rating'>";
