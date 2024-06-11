9e5bf28c2c5ce64687069c685c4475abec3797d7
jonaspgson <jonaspgson@gmail.com>
2024-06-11 02:57:27 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -97,6 +97,30 @@ function createCompactAmazonLinks(productName) {
 }
 
 
+
+function getByline() {
+  let o = '<section class="byline byline-jonas">';
+  o += '<byline-image></byline-image>';
+  o += '<byline-text>';
+  o += '  <byline-title>Images and Words</byline-title>';
+  o += '     <byline-name>Jonas Gustafsson</byline-name>';
+  o += '  <byline-links>';
+  o += '    <a href="https://www.instagram.com/jonas_thejo" target="_blank">';
+  o += '      <i class="fa-brands fa-instagram"></i>';
+  o += '    </a>';
+  o += '    <a href="https://x.com/jonasen" target="_blank">';
+  o += '      <i class="fa-brands fa-x-twitter"></i>';
+  o += '    </a>';
+  o += '    <a href="https://www.youtube.com/@jonasthejo" target="_blank">';
+  o += '      <i class="fa-brands fa-youtube"></i>';
+  o += '    </a>';
+  o += '  </byline-links>';
+  o += '</byline-text>';
+  o += '</section>';
+  document.write(o);
+}
+
+
 /* Light YouTube Embeds by @labnol */
 /* Web: https://www.labnol.org/ */
 
