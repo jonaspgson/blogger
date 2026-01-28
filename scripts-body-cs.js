dd1774addac86812ba1912fbf21e525335084da5
jonaspgson <jonaspgson@gmail.com>
2026-01-28 14:16:46 +0100

    Update scripts-body-cs.js

--- scripts-body-cs.js
+++ scripts-body-cs.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   initTooltips();
   initSplitHeadings();
-  initPublishDate();
+  //initPublishDate();
   initAds();
   initRelatedPosts();
   initAutoRelatedPosts();
